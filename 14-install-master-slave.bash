d8b1d67f3d9ea0fa1e91168d4b326faf9a710fe6
Bruno FROGER <bruno.froger@orange.com>
2024-08-06 14:47:50 +0200

    update script et doc

--- 14-install-master-slave.bash
+++ 14-install-master-slave.bash
@@ -4,8 +4,6 @@
 if [[ "X-$K8S_TYPE_NOEUD" == "X-master" ]]; then
     echo "installation du noeud master ... "
     cde="sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --cri-socket=unix:///var/run/cri-dockerd.sock"
-    echo "commande executee : $cde"
-    $($cde)
 elif [[ "X-$K8S_TYPE_NOEUD" == "X-slave" ]]; then
         echo "installation d'un noeud esclave ... "
         #token=$(kubeadm token create)
@@ -16,8 +14,17 @@ elif [[ "X-$K8S_TYPE_NOEUD" == "X-slave" ]]; then
             --ignore-preflight-errors=all \
             --discovery-token-unsafe-skip-ca-verification \
             --cri-socket=unix:///var/run/cri-dockerd.sock"
-        echo "commande executéé : $cde"
-        $($cde)
 else
     echo "type de noeud ($K8S_TYPE_NOEUD) inconnu"
+fi
+
+echo "commande executee : $cde"
+$($cde)
+result=$?
+echo "le resultat de la commande est $result"
+if [[ "$result" == "0" ]]; then
+    echo "commande executée avec succès .... on continue"
+else
+    echo "commande init/join en erreur .... que faire ?"
+    read -p "Appuyer sur une touche pour continuer ou ^C pour arreter : "
 fi
