0c2e558c01ccefb8769da4fe9ca38ccd0fd009f9
Bruno FROGER <bruno.froger@orange.com>
2024-08-06 15:19:14 +0200

    update script et doc

--- 03-post-install-linux.bash
+++ 03-post-install-linux.bash
@@ -8,14 +8,17 @@ echo "*************************************************"
 if [[ "X-$K8S_SCRIPT_POST_INSTALL_LINUX" == "X-OK" ]]; then
     echo "post installation linux déjà réalisée"
 else 
+    echo "update/upgrade"
     sudo apt-get update 
     sudo apt-get upgrade
 
+    echo "install tools"
     sudo apt install -y net-tools
     sudo apt install -y inetutils-ping
     sudo apt install -y openssh-server
     sudo apt install -y wget
 
+    echo "gestion cle ssh"
     if [ ! -f "~/.ssh/id.rsa.pub"]; then
         echo "génération de la clé ssh"
         ssh-keygen -N ""
