11d4652e430b3d2de79c4bc2baa2f42a60577d1c
Bruno FROGER <bruno.froger@orange.com>
2024-08-06 14:49:55 +0200

    update script et doc

--- 99-reset-init.bash
+++ 99-reset-init.bash
@@ -12,3 +12,7 @@ unset K8S_SCRIPT_POST_INSTALL_LINUX
 
 # srcipt install docker
 unset K8S_SCRIPT_INSTALL_DOCKER
+
+# srcipt install kubeadm
+unset K8S_SCRIPT_INSTALL_KUBEADM
+. ./98-reset-kubeadm.bash
