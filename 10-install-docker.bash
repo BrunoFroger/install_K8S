376736427d03825ab25a17200d427618bbab7870
Bruno FROGER <bruno.froger@orange.com>
2024-08-06 15:25:07 +0200

    update script et doc

--- 10-install-docker.bash
+++ 10-install-docker.bash
@@ -15,7 +15,7 @@ else
   echo "docker => suppression ancienne version de docker"
   for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; 
   do 
-      sudo apt-get remove $pkg; 
+      sudo apt-get -y remove $pkg; 
   done
 
   echo "-------------------------------------------------"
