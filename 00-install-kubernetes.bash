93ab891c58dde69babe91768f41261764b52787c
Bruno FROGER <bruno.froger@orange.com>
2024-08-06 11:06:25 +0200

    update script et doc

--- 00-install-kubernetes.bash
+++ 00-install-kubernetes.bash
@@ -12,21 +12,21 @@ echo "kubernetes => initialisations debut"
 echo "kubernetes => initialisations fin"
 #-------------------------------------------------
 
-read "appuyer sur une touche pour continuer"
+read -p "appuyer sur une touche pour continuer"
 
 #-------------------------------------------------
 #echo "kubernetes => post-install linux"
 . ./03-post-install-linux.bash
 #-------------------------------------------------
 
-read "appuyer sur une touche pour continuer"
+read -p "appuyer sur une touche pour continuer"
 
 #-------------------------------------------------
 #echo "kubernetes => installation de docker"
 . ./10-install-docker.bash
 #-------------------------------------------------
 
-read "appuyer sur une touche pour continuer"
+read -p "appuyer sur une touche pour continuer"
 
 #-------------------------------------------------
 #echo "kubernetes => installation de kubeadm"
