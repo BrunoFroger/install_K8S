4f250210f45a40d51d89aa28dd43c745e746a927
Bruno FROGER <bruno.froger@orange.com>
2024-08-14 10:56:38 +0200

    update doc et scripts

--- 14-install-master-slave.bash
+++ 14-install-master-slave.bash
@@ -3,7 +3,17 @@
 
 if [[ "X-$K8S_TYPE_NOEUD" == "X-master" ]]; then
     echo "installation du noeud master ... "
-    sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --cri-socket=unix:///var/run/cri-dockerd.sock
+    sudo kubeadm init --pod-network-cidr=192.168.0.0/16 \
+    --cri-socket=unix:///var/run/cri-dockerd.sock \
+    > >(tee ./kubeadm-init.out.log) 2>./kubeadm-init.err.log
+    echo "*******************************************************"
+    echo "                                                      *"
+    echo "          A T T E N T I O N                           *"
+    echo "                                                      *"
+    echo "* les informations pour connecter un noued a ce       *"
+    echo "* cluster sont dans le fichier kubeadm-init.out.log   *" 
+    echo " ne pas effacer ce fichier .....                      *" 
+    echo "*******************************************************"
 elif [[ "X-$K8S_TYPE_NOEUD" == "X-slave" ]]; then
         echo "installation d'un noeud esclave ... "
         echo "saisissez le token du master (kubeadm token create) et recopiez le ici : "
