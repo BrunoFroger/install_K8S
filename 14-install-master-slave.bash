88e73f5f90c9379fec7cc100ee6784becafe1471
Bruno FROGER <bruno.froger@orange.com>
2024-08-19 16:43:51 +0200

    update doc et scripts

--- 14-install-master-slave.bash
+++ 14-install-master-slave.bash
@@ -15,10 +15,25 @@ if [[ "X-$K8S_TYPE_NOEUD" == "X-master" ]]; then
     echo " ne pas effacer ce fichier .....                      *" 
     echo "*******************************************************"
 elif [[ "X-$K8S_TYPE_NOEUD" == "X-slave" ]]; then
-        echo "installation d'un noeud esclave ... "
-        echo "saisissez la commande kubeadm join donnée lors de la création du master (fichier kubeadm-init.out.log sur master) : "
-        read cdeJoin
-        sudo sudo ${cdeJoin} --cri-socket=unix:///var/run/cri-dockerd.sock
+    echo "installation d'un noeud esclave ... "
+    while :
+    do
+        echo "saisie de l'adresse et port du master (format xxx.xxx.xxx.xxx:nnnn) (voir avant derniere ligne du fichier kubeadm-init.out.log sur master) : <${addr-master>} "
+        read addr-master
+        echo "executer la cde 'kubeadm token create' sur le master et copier le token ici (exemple : n8o17f.h4r3qlkfuibkl2gd ): <${option-token}> "
+        read option-token
+        echo "saisissez l'option '--discovery-token-ca-cert-hash=sha256:......' de kubeadm join donnée lors de la création du master (derniere ligne du fichier kubeadm-init.out.log sur master) : <${option-cert-hash} "
+        read option-cert-hash
+        cde-join="sudo kubeadm ${addr-master} --token ${option-token} --discovery-token-ca-cert-hash ${option-cert-hash} --cri-socket=unix:///var/run/cri-dockerd.sock"
+        echo "Voici la commande qui va etre executer : "
+        echo $cdeJoin
+        echo "est ce correct ? (o/N)"
+        read valid
+        if [[ "$X-valid" == "X-o" ]] ; then
+            break
+        fi
+    done
+        $(cdeJoin)
 else
     echo "type de noeud ($K8S_TYPE_NOEUD) inconnu"
 fi
