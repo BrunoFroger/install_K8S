489aa0b5595158948c584ebdb7e3fcff17f97b8b
Bruno FROGER <bruno.froger@orange.com>
2024-08-20 11:34:54 +0200

    update doc et scripts

---
+++ 75-get-join-commande.bash
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+function aide() {
+    echo "syntaxe :"
+    echo "$0 <repertoire du fichier d'init kubeadm>"
+}
+
+
+echo "cette commande génère la ligne de commande a utiliser pour joindre une machine au cluser de ce master"
+
+if [[ "x-$1" == "x-" ]]; then
+    echo "erreur : manque parametre"
+    aide()
+    return -1
+fi
+
+
+echo "vous etes sur la machine : " $(hosname)
+echo "le répertoire sur master contenant le fichier spécifiant la commande est : " 
+cd $1
