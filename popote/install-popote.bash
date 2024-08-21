3f20809543a040cd44a7917df7fd7b20474c316f
Bruno FROGER <bruno.froger@orange.com>
2024-08-21 16:12:27 +0200

    ajout du fchier d'installation de popote

---
+++ popote/install-popote.bash
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+#creation du namespace popote
+kubectl create namespace popote
+
+
+# deployment du pod multicontaineur de popote
+kubectl create deployment deployment-monopod.yaml -n popote
+
