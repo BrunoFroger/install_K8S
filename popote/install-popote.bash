fdef53939f264d21bb2066b8574718c939dbf159
Bruno FROGER <bruno.froger@orange.com>
2024-08-28 14:26:04 +0200

    test deployement popote

--- popote/install-popote.bash
+++ popote/install-popote.bash
@@ -1,21 +1,4 @@
-#!/bin/bash -e
-
-# installation de kubectl-convert si necessaire)
-# testConvert=$(whereis kubectl-convert | grep /usr/local/bin | wc -l)
-# if [[ $testConvert == 0 ]]; then 
-#     echo "installation de kubectl-convert"
-#     # 1 Download the latest release with the command:
-#     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl-convert"
-#     # 2 Validate the binary (optional)
-#     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl-convert.sha256"
-#     echo "$(cat kubectl-convert.sha256) kubectl-convert" | sha256sum --check
-#     #Validate the kubectl-convert binary against the checksum file: (reponse attendue => kubectl-convert: Réussi)
-#     echo "$(cat kubectl-convert.sha256) kubectl-convert" | sha256sum --check
-#     #l'executable est dans le chemin courant ; vous pouvez eventuellement le deplacer vers ver /usr/local/bin pour qu'il soit utilisable partout
-#     sudo mv kubectl-convert /usr/local/bin/
-# else
-#     echo "kubectl-convert deja installé"
-# fi
+#!/bin/bash
 
 # test si les images docker popote existent
 while :
@@ -23,8 +6,8 @@ do
     imagesPopote=$(docker images | grep popote | wc -l)
     if [[ $imagesPopote == 0 ]]; then
         echo "Les images popote n'existent pas, il faut les creer !"
-        mkdir popote
-        cd popote
+        mkdir popote_files
+        cd popote_files
         wget https://github.com/BrunoFroger/popote_vueJS_K8S/releases/latest > wget.log 2>&1 
         rm latest
         archive="$(cat wget.log | grep release | tail -1 | awk -F' ' '{print $NF}')"
@@ -38,8 +21,9 @@ do
         echo "fichier = $fichier"
         unzip $fichier
         rm $fichier
-        cd popote_vueJS_K8S
+        cd popote_vueJS_K8S$version
         echo "construction des images docker popote en cours ....."
+        read "appuyer sur une touche"
         docker compose build
 
         # push images in local repository
