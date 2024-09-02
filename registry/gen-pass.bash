1130593374eb00779c2e3186d95f7ce874578f85
Bruno FROGER <bruno.froger@orange.com>
2024-09-02 10:45:47 +0200

    test create repository

---
+++ registry/gen-pass.bash
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+export REGISTRY_USER=K8Sregistry
+export REGISTRY_PASS="K8S&registry"
+export DESTINATION_FOLDER=./registry-creds
+   
+# Backup credentials to local files (in case you'll forget them later on)
+mkdir -p ${DESTINATION_FOLDER}
+echo ${REGISTRY_USER} >> ${DESTINATION_FOLDER}/registry-user.txt
+echo ${REGISTRY_PASS} >> ${DESTINATION_FOLDER}/registry-pass.txt
+   	
+docker run --entrypoint htpasswd registry:2.7.0 \
+    -Bbn ${REGISTRY_USER} ${REGISTRY_PASS} \
+    > ${DESTINATION_FOLDER}/htpasswd
+      
+unset REGISTRY_USER REGISTRY_PASS DESTINATION_FOLDER
