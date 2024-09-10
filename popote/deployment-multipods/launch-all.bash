3200bc14aa7f4b387dd1f568ab4040ff60ece4fb
Bruno FROGER <bruno.froger@orange.com>
2024-09-10 16:12:56 +0200

    update build popote multipods

--- popote/deployment-multipods/launch-all.bash
+++ popote/deployment-multipods/launch-all.bash
@@ -19,6 +19,7 @@ podId=$(kubectl get pods | grep ${module} | awk -F ' ' '{print $1}')
 cpt=0
 while :
 do
+    # definir dans la ligne ci dessous les condiftion de fin d'initialisation
     result=$(kubectl logs ${podId} | tail -2 | head -1 | grep "ready for connections" | wc -l ) 
     if [[ "X-${result}" == "X-1" ]]; then 
         break
@@ -36,29 +37,3 @@ echo ""
 echo "${module} démarré avec succès"
 echo "====================================="
 
-
-echo "====================================="
-module="backend"
-echo "lancement de ${module}"
-kubectl apply -f deployment-${module}.yaml
-sleep 5
-podId=$(kubectl get pods | grep ${module} | awk -F ' ' '{print $1}')
-cpt=0
-while :
-do
-    result=$(kubectl logs ${podId} | tail -2 | head -1 | grep "TODO : a definir" | wc -l ) 
-    if [[ "X-${result}" == "X-1" ]]; then 
-        break
-    else
-        echo -n "."
-    fi
-    sleep 5
-    ((cpt++))
-    if [[ ${cpt} -gt 10 ]]; then
-        echo "delai pour demarrer dépassé"
-        exit -1
-    fi
-done
-echo ""
-echo "mariadb démarré avec succès"
-echo "====================================="
