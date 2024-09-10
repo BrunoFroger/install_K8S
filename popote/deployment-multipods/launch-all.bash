d207164378e73acdd33eb50c3243b0ff883e8e6f
Bruno FROGER <bruno.froger@orange.com>
2024-09-10 16:00:18 +0200

    update build popote multipods

--- popote/deployment-multipods/launch-all.bash
+++ popote/deployment-multipods/launch-all.bash
@@ -15,15 +15,21 @@ echo "lancement de mariadb"
 kubectl apply -f deployment-mariadb.yaml
 sleep 5
 podId=$(kubectl get pods | grep mariadb | awk -F ' ' '{print $1}')
+cpt=0
 while :
 do
-    result=$(kubectl logs ${podId} | tail -2 | head -1 | grep "ready for connections" | wc -l) 2>&1>/dev/null
+    result=$(kubectl logs ${podId} | tail -2 | head -1 | grep "ready for connections" | wc -l ) 
     if [[ "X-${result}" == "X-1" ]]; then 
         break
     else
         echo -n "."
     fi
     sleep 5
+    cpt=$(cpt + 1)
+    if [[ ${cpt} > 2 ]]; then
+        echo "delai pour demarrer dépassé"
+        exit-1
+    fi
 done
 echo ""
 echo "mariadb démarré avec succès"
