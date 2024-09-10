16afd6613fdfb724fd9ff09c8ba3113b7f7eea57
Bruno FROGER <bruno.froger@orange.com>
2024-09-10 16:15:10 +0200

    update build popote multipods

--- popote/deployment-multipods/launch-all.bash
+++ popote/deployment-multipods/launch-all.bash
@@ -29,7 +29,35 @@ do
     sleep 5
     ((cpt++))
     if [[ ${cpt} -gt 10 ]]; then
-        echo "delai pour demarrer dépassé"
+        echo "delai pour demarrer ${module} dépassé"
+        exit -1
+    fi
+done
+echo ""
+echo "${module} démarré avec succès"
+echo "====================================="
+
+
+echo "====================================="
+module="backend"
+echo "lancement de ${module}"
+kubectl apply -f deployment-${module}.yaml
+sleep 5
+podId=$(kubectl get pods | grep ${module} | awk -F ' ' '{print $1}')
+cpt=0
+while :
+do
+    # definir dans la ligne ci dessous les condiftion de fin d'initialisation
+    result=$(kubectl logs ${podId} | tail -2 | head -1 | grep "TODO fixer condition de fin d'init" | wc -l ) 
+    if [[ "X-${result}" == "X-1" ]]; then 
+        break
+    else
+        echo -n "."
+    fi
+    sleep 5
+    ((cpt++))
+    if [[ ${cpt} -gt 10 ]]; then
+        echo "delai pour demarrer ${module} dépassé"
         exit -1
     fi
 done
