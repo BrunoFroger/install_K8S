fe780e3347358a3d0a992a9f9e1407e9f92dbf8a
Bruno FROGER <bruno.froger@orange.com>
2024-09-13 09:39:55 +0200

    update build popote multipods

---
+++ popote/deployment-multipods/show-logs.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ $# ne 1 ]]; then
+    echo "manque nom du pod pour afficher ses logs"
+    exit -1
+fi
+
+POD=$(kubectl get pods | grep $1 | awk -F ' ' '{print $1}')
+kubectl logs -f $POD
