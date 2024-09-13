1c2f581a477b2f85df5913a189e31f4c3d6549cb
Bruno FROGER <bruno.froger@orange.com>
2024-09-13 09:48:14 +0200

    update build popote multipods

--- popote/deployment-multipods/show-logs.bash
+++ popote/deployment-multipods/show-logs.bash
@@ -7,6 +7,7 @@ fi
 
 POD=$(kubectl get pods | grep $1 | awk -F ' ' '{print $1}')
 nbPods=$(echo $POD | wc -l)
+echo "nbPods = $nbPods"
 if [[ $nbPods -gt 1 ]]; then 
     echo "$1 ne correspond pas a un pod unique, veuillez affiner votre selection"
     exit -1
