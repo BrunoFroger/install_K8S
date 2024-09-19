fe60fea1e6f525d3f0eaa20a1afc4cc8eeb20dc2
Bruno FROGER <bruno.froger@orange.com>
2024-09-19 16:01:10 +0200

    update build popote multipods

---
+++ popote/deployment-multipods/connect-pos.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+if [[ "X-$1" == "X-" ]]; then   
+    echo "manque nom du pod (mariadb / backend / nginx / frontend)"
+    exit -1
+fi
+
+podId=$(kubectl get pods | grep $1 |  | awk -F ' ' '{print $1}')
+if [[ "X-$podId" == "X-" ]]; then
+    echo "le pod $1 n'existe pas"
+    exit -1
+fi
+
+kubectl exec -it $podId bash
