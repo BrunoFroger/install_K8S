1411e08be3a44ad03971adb569fb83ccb9e372de
Bruno FROGER <bruno.froger@orange.com>
2024-09-19 22:37:08 +0200

    test launch multipods avec frontend

--- popote/deployment-multipods/launch-all.bash
+++ popote/deployment-multipods/launch-all.bash
@@ -87,3 +87,35 @@ echo ""
 echo "${module} démarré avec succès"
 echo "====================================="
 
+echo "====================================="
+module="frontend"
+sed "s^{{IMAGE}}^fbruno/popote_vuejs_k8s-tags-${TAG}-${module}:latest^" deployment-${module}-copy.yaml > deployment-${module}.yaml
+echo "lancement de ${module}"
+POD_BACKEND=$(kubectl get pods | grep backend | head -1 | awk -F ' ' '{print $1}') ; echo "pod mariadb = $POD_BACKEND"
+IP_BACKEND=$(kubectl describe pods $POD_BACKEND | egrep "^IP:" | tail -1 | awk -F ' ' '{print $NF}'); echo "IP mariadb = $IP_BACKEND"
+sed -i "s/{{IP_MARIADB}}/${IP_BACKEND}/" deployment-${module}.yaml
+kubectl apply -f deployment-${module}.yaml
+sleep 5
+podId=$(kubectl get pods | grep ${module} | awk -F ' ' '{print $1}')
+echo "podId de $module = $podId"
+cpt=0
+while :
+do
+    # definir dans la ligne ci dessous les condiftion de fin d'initialisation
+    result=$(kubectl logs ${podId} | grep "Connecté à la base de données MySQL Popote !" | wc -l ) 
+    if [[ "X-${result}" == "X-1" ]]; then 
+        break
+    else
+        echo -n "."
+    fi
+    sleep 2
+    ((cpt++))
+    if [[ ${cpt} -gt 30 ]]; then
+        echo "delai pour demarrer ${module} dépassé"
+        exit -1
+    fi
+done
+echo ""
+echo "${module} démarré avec succès"
+echo "====================================="
+
