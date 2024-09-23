b193ca17c9524ea75119e1ae3774be10e31ffebd
Bruno FROGER <bruno.froger@orange.com>
2024-09-23 14:53:34 +0200

    test pod nginx

--- pod-nginx/sidecars/build-sidecars.bash
+++ pod-nginx/sidecars/build-sidecars.bash
@@ -1,3 +1,12 @@
 #!/bin/bash -e
 
 docker build -t monapp:0.1 .
+
+docker run --name monapp -d -p 5000:5000 monapp:0.1
+
+kubectl create namespace monapp
+kubectl config set-context --current --namespace=monapp
+
+kubectl create configmap monapp-nginx --from-file=nginx.conf
+
+kubectl apply -f app-deployment.yml
