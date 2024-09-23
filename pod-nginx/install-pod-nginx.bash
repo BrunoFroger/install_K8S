bbc130f75b7fdcf60a6ff5c2d049327dedf9ab91
Bruno FROGER <bruno.froger@orange.com>
2024-09-23 14:39:31 +0200

    test pod nginx

--- pod-nginx/install-pod-nginx.bash
+++ pod-nginx/install-pod-nginx.bash
@@ -1,4 +1,12 @@
 #!/bin/bash -e
 
+kubectl config set-context --current --namespace=ingress-nginx
 
-kubectl apply -f ./nginx-deployment.yaml
+# Installons le gestionnaire d’Ingress Nginx
+#kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/baremetal/deploy.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.3.0/deploy/static/provider/cloud/deploy.yaml
+
+# patchons le déploiement du contrôleur nginx 
+kubectl patch deployment ingress-nginx-controller -n ingress-nginx -p '{"spec":{"template":{"spec":{"hostNetwork":true}}}}'
+
+#kubectl apply -f ./nginx-deployment.yaml
