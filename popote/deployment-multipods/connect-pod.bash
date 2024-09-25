d7ca9c9f4d869d39f79a863fa4486117fb5eb0c3
Bruno FROGER <bruno.froger@orange.com>
2024-09-25 15:50:19 +0200

    reprise tests monopod

--- popote/deployment-multipods/connect-pod.bash
+++ popote/deployment-multipods/connect-pod.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 aide(){    
-    echo "manque nom du pod (mariadb / backend / nginx / frontend)"
     echo "syntaxe : connect-pod.bash pod [container]"
-    echo "          pod = chaine minimale permaettant d'dentifier un pod"
-    echo "          container = (si plusieurs container dans un pod) chaine minimale permaettant d'dentifier un container"
+    echo "          pod = chaine minimale permaettant d'identifier un pod"
+    echo "          container = (si plusieurs container dans un pod) chaine minimale permaettant d'identifier un container"
     exit -1
 }
 
 if [[ "X-$1" == "X-" ]]; then   
+    echo "ERREUR : manque nom du pod (mariadb / backend / nginx / frontend)"
     aide
 fi
 
@@ -16,24 +16,24 @@ podId=$(kubectl get pods | grep $1 | awk -F ' ' '{print $1}')
 echo "podId = $podId"
 
 if [[ "X-$podId" == "X-" ]]; then
-    echo "le pod $1 n'existe pas"
+    echo "ERREUR : le pod $1 n'existe pas"
     aide
 fi
 
 containerListe=$(kubectl get pods $podId -o='custom-columns=CONTAINERS:.spec.containers[*].name'| tail -1 | sed 's/,/\n/g')
-echo "Liste des containeurs = $containerListe"
+#echo "Liste des containeurs = $containerListe"
 nbContainers=$(echo $containerListe | wc -w)
-echo "nombre des containeurs = $nbContainers"
+#echo "nombre des containeurs = $nbContainers"
 
 containeur=""
 if [[ $nbContainers -gt 1 ]]; then
-    echo "le pod $podId contient plusieurs containeurs"
+    #echo "le pod $podId contient plusieurs containeurs"
     if [[ "X-$2" != "X-" ]]; then   
         containerId=$(echo "$containerListe" | grep $2)
-        echo "containeur sur lequel on se connecte : $containerId"
+        #echo "containeur sur lequel on se connecte : $containerId"
         containeur="--container $containerId"
     else 
-        echo "erreur ce pod contient plusieurs container, il faut preciser sur quel container se connecter"
+        echo "ERREUR : ce pod contient plusieurs container, il faut preciser sur quel container se connecter"
         aide
     fi
 fi
