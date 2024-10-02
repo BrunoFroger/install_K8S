cb1a4347b20ed0e1346bf3c0653a5eaaa29e19d2
Bruno FROGER <bruno.froger@orange.com>
2024-10-02 13:48:50 +0200

    test ingress

--- install-ingress-nginx/install-ingress.bash
+++ install-ingress-nginx/install-ingress.bash
@@ -4,18 +4,15 @@
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.11.2/deploy/static/provider/cloud/deploy.yaml
 
 # attendre fin d'ainstall (running)
-kubectl wait --namespace ingress-nginx \
-  --for=condition=ready pod \
-  --selector=app.kubernetes.io/component=controller \
-  --timeout=120s
+kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=120s
 
 # creation d'un serveur http provisoire pour test
-kubectl create deployment demo --image=httpd --port=80
-kubectl expose deployment demo
+#kubectl create deployment demo --image=httpd --port=80
+#kubectl expose deployment demo
 
 # creation d'une ressource ingress associee
-kubectl create ingress demo-localhost --class=nginx \
-  --rule="popote.zapto.org/*=demo:80"
+#kubectl create ingress demo-localhost --class=nginx --rule="popote.zapto.org/*=demo:80"
+kubectl create ingress popote-localhost --class=nginx --rule="popote.zapto.org/*=deployment-popote-monopod:80"
 
 # activation du port forward pour acceder a ce site
 kubectl port-forward --namespace=ingress-nginx service/ingress-nginx-controller 8080:80
