3b7037ef182abb350c56c72a67e67041f35d81bc
Bruno FROGER <bruno.froger@orange.com>
2024-10-03 11:35:36 +0200

    test ingress

--- popote/status-deployment.bash
+++ popote/status-deployment.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Pod    : " $(kubectl get pods | tail -1)
-kubectl get deployments.apps
-kubectl get services
-kubectl get ingress
+echo "Pod         : " $(kubectl get pods | tail -1)
+echo "Deployments : " $(kubectl get deployments.apps | tail -1)
+echo "Service     : " $(kubectl get services | tail -1)
+echo "Ingress     : " $(kubectl get ingress | tail -1)
