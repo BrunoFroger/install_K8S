f970216706a88c1d70b3458dbd400017e000fd26
Bruno FROGER <bruno.froger@orange.com>
2024-10-03 10:38:18 +0200

    test ingress

---
+++ popote/clean-deployement.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+exec-cde(){
+    echo "suppresion de $1"
+    result=$(kubectl get $1 | grep popote | wc -l)
+    if [[ $result >= 1 ]]; then
+        result=$(kubectl get $1 | grep popote | awk -F '{print $1}')
+        kubectl delete $1s $result
+    fi
+}
+
+listeitems=("deployments.apps", "services", "ingress")
+
+for item in $listeItems
+do
+    exec-cde($item)
+done
+
