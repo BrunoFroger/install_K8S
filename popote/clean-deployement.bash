29038583a03d77d26c646e3aa57d4151e019c65b
Bruno FROGER <bruno.froger@orange.com>
2024-10-03 11:16:14 +0200

    test ingress

--- popote/clean-deployement.bash
+++ popote/clean-deployement.bash
@@ -7,12 +7,15 @@ exec-cde(){
         ressource=$(kubectl get $1 | grep popote | awk -F ' ' '{print $1}')
         echo "suppression de la ressource $ressource"
         kubectl delete $1 $ressource
+        echo "on attends la fin de l'arret de $1"
         while :
         do
-            echo "on attends la fin de l'arret de $1"
+            echo -n "."
+            sleep 2
             result=$(kubectl get $1 | grep popote | wc -l)
             if [[ $result -eq 0 ]]; then
-                continue
+                echo ""
+                break;
             fi
         done
     fi
