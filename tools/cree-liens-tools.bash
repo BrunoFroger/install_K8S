6f36ea3ed4915b2e65446cbd4a9385a9965a5fbd
Bruno FROGER <bruno.froger@orange.com>
2024-10-04 15:57:49 +0200

    test ingress

--- tools/cree-liens-tools.bash
+++ tools/cree-liens-tools.bash
@@ -10,8 +10,8 @@ listeFichiers=$(ls *bash | grep -v cree-liens)
 for fichier in $listeFichiers
 do
     echo "check de $fichier"
-    if [ ! -f ${HOME}/bin/$fichier ]; then
-        echo "creation du lien pour $fichier"
-        ln -s $fichier ${HOME}/bin/
+    if [ ! -f ${HOME}/bin/${fichier} ]; then
+        echo "creation du lien pour ${fichier} vers ${HOME}/bin/${fichier}"
+        ln -s ${fichier} ${HOME}/bin/${fichier}
     fi
 done
