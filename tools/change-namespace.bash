c3247991a44bfe05f207814ce4bbfad7b9499c9e
Bruno FROGER <bruno.froger@orange.com>
2024-10-04 14:15:04 +0200

    test ingress

--- tools/change-namespace.bash
+++ tools/change-namespace.bash
@@ -6,6 +6,13 @@ if [ $# -ne 1 ]; then
     echo "Commande attendue : ./$0 <namespace>" 
     exit
 else
+    # test que le namespace existe
+    result=$(kubectl get namespaces | grep $1 | wc -l)
+    if [ $result -ne 1 ]; then
+        echo "le namespce $1 n'existe pas"
+        exit
+    fi
+    echo "on change de namespace : $1"
     export K8S_NAMESPACE=$1
     set-bash-variable.bash K8S_NAMESPACE=$1
     kubectl config set-context --current --namespace=$K8S_NAMESPACE
