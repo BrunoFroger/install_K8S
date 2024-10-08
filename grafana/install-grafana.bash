fbd4a1616b18f1c5a8eff8d4b2c4a3f6b4536c43
Bruno FROGER <bruno.froger@orange.com>
2024-10-08 15:50:08 +0200

    installation grafana

--- grafana/install-grafana.bash
+++ grafana/install-grafana.bash
@@ -1,6 +1,10 @@
 #!/bin/bash -e
 
+# cf doc : https://grafana.com/docs/grafana/latest/setup-grafana/installation/kubernetes/
 
 #creation du namespace grafana
 kubectl create namespace my-grafana
+
+
+kubectl apply -f grafana.yaml --namespace=my-grafana
 
