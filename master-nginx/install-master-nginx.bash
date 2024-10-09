6dab4796f0b374fe7a447e5419862bb064024c43
Bruno FROGER <bruno.froger@orange.com>
2024-10-09 15:34:26 +0200

    test master-nginx

--- master-nginx/install-master-nginx.bash
+++ master-nginx/install-master-nginx.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# https://blog.zwindler.fr/2018/03/06/exposer-des-applications-kubernetes-en-dehors-des-cloud-providers-nginx-ingress-controller/
 
 # creation du namespace master-nginx
 kubectl create namespace master-nginx
 change-namespace.bash master-nginx
 
+# deploiement ....
 kubectl apply -f master-service.yaml
 kubectl apply -f master-ingress.yaml
