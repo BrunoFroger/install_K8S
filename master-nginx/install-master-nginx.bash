05209461909c416f0d127db4ffce588a25908983
Bruno FROGER <bruno.froger@orange.com>
2024-10-09 14:09:04 +0200

    test master-nginx

--- master-nginx/install-master-nginx.bash
+++ master-nginx/install-master-nginx.bash
@@ -3,6 +3,7 @@
 
 # creation du namespace master-nginx
 kubectl create namespace master-nginx
-
 change-namespace.bash master-nginx
 
+kubectl apply -f master-service.yaml
+kubectl apply -f master-ingress.yaml
