4d7ab4820a506ce40eaa2722144f3cdc66d9d1b2
Bruno FROGER <bruno.froger@orange.com>
2024-12-23 14:10:27 +0100

    test ingress-nginx

--- pod-nginx/install-pod-nginx.bash
+++ pod-nginx/install-pod-nginx.bash
@@ -10,3 +10,13 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/cont
 kubectl patch deployment ingress-nginx-controller -n ingress-nginx -p '{"spec":{"template":{"spec":{"hostNetwork":true}}}}'
 
 #kubectl apply -f ./nginx-deployment.yaml
+
+# activation du port forward pour acceder a ce site
+kubectl port-forward --namespace=ingress-nginx service/ingress-nginx-controller 8080:80
+# kubectl port-forward --namespace=ingress-nginx service/ingress-nginx-controller 80:80
+
+# test 
+echo "test : doit afficher 'Itworks!'"
+curl --resolve k8sbfr.zapto.org:8080:127.0.0.1 http://k8sbfr.zapto.org:8080
+# curl --resolve k8sbfr.zapto.org:80:127.0.0.1 http://k8sbfr.zapto.org
+echo ""
