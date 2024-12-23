c109936a91c7c16ea199f015596afcc10e322e71
Bruno FROGER <bruno.froger@orange.com>
2024-12-23 14:30:13 +0100

    test ingress-nginx

--- 76-set-all-slaves-worker.bash
+++ 76-set-all-slaves-worker.bash
@@ -6,6 +6,7 @@ echo "liste des esclaves : ${slaves}"
 
 for  slave in ${slaves} ; do
 
-    echo "traitement de ${slave}"
+    echo "set ${slave} worker"
+    kubectl label node ${slave} node-role.kubernetes.io/worker=worker
 
 done
