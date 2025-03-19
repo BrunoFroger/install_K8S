97b880e311e43e600513efb7e441f55af269f5cb
Bruno FROGER <bruno.froger@orange.com>
2025-03-19 15:12:25 +0100

    update scripts install

--- 01-initialisations.bash
+++ 01-initialisations.bash
@@ -61,10 +61,11 @@ master Kubernetes : $K8S_MASTER_KUBERNETES\n\
 master namespace  : $K8S_NAMESPACE\n\n\n\
 valid  : $valid\n\
 est-ce que ces donnees sont exactes :" 12 50 3>&1 1>&2 2>&3)
-        echo "valid = <$valid>"; sleep 2;
         if [[  "X-$valid" == "X-0" ]]; then
             break
         fi
+        echo "valid = <$valid>"
+        sleep 2
     done
 
     source set-bash-variable.bash K8S_SCRIPT_INITIALISATION="OK"
