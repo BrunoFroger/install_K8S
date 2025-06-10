20efb467511b53e2f566b3d85c0511d747ea2e1f
Bruno FROGER <bruno.froger@orange.com>
2025-06-10 15:33:32 +0200

    ajout script installation d'applications

--- 30-install-hello-node.bash
+++ 30-install-hello-node.bash
@@ -7,7 +7,23 @@ echo "*                 hello-node"
 echo "*"
 echo "*************************************************"
 
-echo "installation de l'application hello-node"
+#creation du namespace k8sbfr-hello-node
+kubectl create namespace k8sbfr-hello-node
+kubectl config set-context --current --namespace=k8sbfr-hello-node
+
+#creation du deployement k8sbfr-hello-node
+kubectl create deployment k8sbfr-hello-node --image=registry.k8s.io/e2e-test-images/agnhost:2.39 -- /agnhost netexec --http-port=8080
+        # - verifier sa creation avec : kubectl get deployments
+kubectl wait --for=condition=Available deployment/k8sbfr-hello-node --timeout=120s
+
+#exposer le service
+kubectl apply -f svc-hello-node.yaml
+    # - test local :
+    #     - recuperer adresse ip de l'application avec "kubectl describe svc k8sbfr-hello-node"
+    #     - adresse ip dans le champ EndPoints: sous forme ip:port
+    #     - lancer un browser web avec cette adresse ip:port
+    #     - le resultat doit donner date et heure
+echo "installation de l'application hello-node ok"
 read
 
 
