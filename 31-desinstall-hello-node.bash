4744bbca2ac7154e527211380487c31eaca7b733
Bruno FROGER <bruno.froger@orange.com>
2025-06-10 16:20:45 +0200

    ajout script installation d'applications

--- 31-desinstall-hello-node.bash
+++ 31-desinstall-hello-node.bash
@@ -10,10 +10,10 @@ echo "*************************************************"
 if (whiptail --title "Confirmation Oui / Non" --yesno "voulez vous réellement desinstaller hello-node ?" 10 60) then
 
     kubectl config set-context --current --namespace=k8sbfr-hello-node
-    kubectl delete services k8sbfr-hello-node -wait
-    kubectl delete deployment k8sbfr-hello-node -wait
+    kubectl delete services k8sbfr-hello-node --wait
+    kubectl delete deployment k8sbfr-hello-node --wait
     kubectl config set-context --current --namespace=default
-    kubectl delete namespace k8sbfr-hello-node -wait
+    kubectl delete namespace k8sbfr-hello-node --wait
 
     unset K8S_HELLO_NODE_INSTALLED
     echo "hello-node desinstallé"
