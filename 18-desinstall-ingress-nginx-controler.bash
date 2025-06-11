2dc6ce684f53d668bfce954025963fb448c4f807
Bruno FROGER <bruno.froger@orange.com>
2025-06-11 10:32:59 +0200

    ajout script installation d'applications

--- 18-desinstall-ingress-nginx-controler.bash
+++ 18-desinstall-ingress-nginx-controler.bash
@@ -9,10 +9,10 @@ echo "*************************************************"
 
 if (whiptail --title "Confirmation Oui / Non" --yesno "voulez vous réellement desinstaller k8sbfr-my-ingress ?" 10 60) then
 
-    change-namespace.bash default
     kubectl delete deployments.apps ingress-nginx-controller
     kubectl delete services ingress-nginx-controller ingress-nginx-controller-admission
     kubectl delete jobs.batch ingress-nginx-admission-create ingress-nginx-admission-patch 
+    change-namespace.bash default
     kubectl delete namespace ingress-nginx
 
 fi
