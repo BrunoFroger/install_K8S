21348058e9b08b0a94c2ace89be2582d5361e2d6
Bruno FROGER <bruno.froger@orange.com>
2025-06-12 16:05:18 +0200

    ajout script installation d'applications

--- 18-install-ingress-nginx-controler.bash
+++ 18-install-ingress-nginx-controler.bash
@@ -16,20 +16,21 @@ if [[ $(kubectl get namespaces 2> /dev/null | grep ingress-nginx | wc -l) == 0 ]
     echo "creation du namespace en cours ....."
     kubectl create namespace ingress-nginx
 fi
+change-namespace.bash ingress-nginx
 
 echo "essai installation de ingress-controller"
 if [[ $(kubectl get deployements.app 2> /dev/null | grep -v NAME | grep ingress-nginx-controller | wc -l) == 0 ]]; then
     # installation ingress controleur
     kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.11.2/deploy/static/provider/cloud/deploy.yaml
     # attendre fin d'install (running)
-    externalIp=$(ifconfig eno1 | grep "inet "| awk '{ print $2 }')
-    echo "l'adresse IP externe utilisée est : $externalIp"
     #  TODO
     # commande de modification du fichier yaml de service pour ajouter externalIP: $externalIP
     # kubectl -n ingress-nginx patch svc ingress-nginx-controller --type=json \
     #     -p='[{"op": "add", "path": "/spec/-", "value": "externalIPs: 192.168.1.25"}]'
     # kubectl get svc ingress-nginx-controller -o yaml | sed -s 
     kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=120s
+    externalIp=$(ifconfig eno1 | grep "inet "| awk '{ print $2 }')
+    echo "l'adresse IP externe utilisée est : $externalIp"
     kubectl get services ingress-nginx-controller -o yaml > service-ingress-nginx-controller.yaml
     awk -v extIp=$externalIp '/spec:/ { print; print "  externalIPs:"; print  "    - " extIp; next }1' service-ingress-nginx-controller.yaml | kubectl apply -f -
     rm service-ingress-nginx-controller.yaml
