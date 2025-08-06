cc017e62039026774add80924d2abf4affbef23e
Bruno FROGER <bruno.froger@orange.com>
2025-08-06 15:46:05 +0200

    mise a jour process install kubernetes

--- 19-install-applications.bash
+++ 19-install-applications.bash
@@ -36,7 +36,7 @@ do
     COLUMNS=$(tput cols) 
     texte="choisissez quelle application vous \nvoulez installer/desinstaller : " 
     message=$(printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$texte")
-    nomApplication=$(whiptail --menu "" 15 80 $nbApp ${whiptailContenu} 3>&1 1>&2 2>&3)
+    nomApplication=$(whiptail --menu "$message" 15 80 $nbApp ${whiptailContenu} 3>&1 1>&2 2>&3)
     nomApplication=$(echo $nomApplication | cut -d'"' -f2)
 
     if [[ "X-$nomApplication" != "X-" ]]; then
@@ -60,67 +60,6 @@ do
     fi
     # read
 done
-
-# echo " A FINIR DE DEVELLOPER"
-# read
-
-
-
-# while :
-# do
-
-#     if [[ "X-$K8S_HELLO_NODE_INSTALLED" != "X-" ]] then 
-#         applicationHelloNode="installée"
-#     else
-#         applicationHelloNode=""
-#     fi 
-#     if [[ "X-$K8S_POPOTE_INSTALLED" != "X-" ]] then 
-#         applicationPopote="installée"
-#     else
-#         applicationPopote=""    
-#     fi 
-#     if [[ "X-$K8S_ETHERPAD_INSTALLED" != "X-" ]] then 
-#         applicationEtherpad="installée"
-#     else
-#         applicationEtherpad=""    
-#     fi 
-
-#     nomApplication=$(whiptail --menu "choisissez quelle application vous voulez installer : " 15 60 5 \
-#         "hello" "$applicationHelloNode " \
-#         "popote" "$applicationPopote " \
-#         "etherpad" "$applicationEtherpad " \
-#         "quitter" "" \
-#         3>&1 1>&2 2>&3)
-
-#     if [[ "X-$nomApplication" != "X-" ]]; then
-#         if [[ "$nomApplication" == "quitter" ]]; then
-#             break;
-#         elif [[ "$nomApplication" == "hello" ]]; then
-#             if [[ "X-$applicationHelloNode" == "X-" ]]; then 
-#                 source ./30-install-hello.bash
-#             else
-#                 source ./31-desinstall-hello.bash
-#             fi
-
-#         elif [[ "$nomApplication" == "popote" ]]; then
-#             if [[ "X-$applicationPopote" == "X-" ]]; then 
-#                 source ./30-install-popote.bash
-#             else
-#                 source ./31-desinstall-popote.bash
-#             fi
-
-#         elif [[ "$nomApplication" == "etherpad" ]]; then
-#             if [[ "X-$applicationEtherpad" == "X-" ]]; then 
-#                 source ./30-install-etherpad.bash
-#             else
-#                 source ./31-desinstall-etherpad.bash
-#             fi
-
-#         else
-#             echo "saisie incorrecte !"
-#         fi
-#     fi
-# done
 
 echo "installation des applications => fin"
 
