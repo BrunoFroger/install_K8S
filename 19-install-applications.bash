8fc4bc31ed792220d09961c0418d2388c9246179
Bruno FROGER <bruno.froger@orange.com>
2025-08-06 15:55:04 +0200

    mise a jour process install kubernetes

--- 19-install-applications.bash
+++ 19-install-applications.bash
@@ -35,7 +35,7 @@ do
     whiptailContenu=${whiptailContenu}'"Quitter" ""'
     COLUMNS=$(tput cols) 
     texte="choisissez quelle application vous \nvoulez installer/desinstaller : " 
-    message=$(printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$texte")
+    message=$(printf "<%*s\n" $(((${#title}+$COLUMNS)/2)) "$texte>")
     nomApplication=$(whiptail --menu "$message" 15 80 $nbApp ${whiptailContenu} 3>&1 1>&2 2>&3)
     nomApplication=$(echo $nomApplication | cut -d'"' -f2)
 
