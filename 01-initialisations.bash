2fd66b1deffc96de9555b5af9492d272577fdbec
Bruno FROGER <bruno.froger@orange.com>
2025-08-08 09:50:59 +0200

    mise a jour process install kubernetes

--- 01-initialisations.bash
+++ 01-initialisations.bash
@@ -45,10 +45,10 @@ else
         fi
         # echo -n "nom du namespace a utiliser <$K8S_NAMESPACE> : "
         # read namespace
-        namespace=$(whiptail --inputbox "nom du namespace a utiliser : " 10 50 "$K8S_NAMESPACE" 3>&1 1>&2 2>&3)
-        if [[ "X-$namespace" != "X-" ]]; then
-            source set-bash-variable.bash K8S_NAMESPACE=$namespace
-        fi
+        # namespace=$(whiptail --inputbox "nom du namespace a utiliser : " 10 50 "$K8S_NAMESPACE" 3>&1 1>&2 2>&3)
+        # if [[ "X-$namespace" != "X-" ]]; then
+        #     source set-bash-variable.bash K8S_NAMESPACE=$namespace
+        # fi
 
         # echo "type install      : $K8S_TYPE_NOEUD"
         # echo "master Kubernetes : $K8S_MASTER_KUBERNETES"
@@ -57,8 +57,7 @@ else
         # echo -n "est-ce que ces donnees sont exactes : (o/N) : "
         # read valid
         valid_install=$(whiptail --yesno "type install      : $K8S_TYPE_NOEUD\n\
-master Kubernetes : $K8S_MASTER_KUBERNETES\n\
-master namespace  : $K8S_NAMESPACE\n\n\n\
+master Kubernetes : $K8S_MASTER_KUBERNETES\n\n\n\
 est-ce que ces donnees sont exactes :" 12 50 3>&1 1>&2 2>&3 ; echo $?)
         if [[  "X-$valid_install" == "X-0" ]]; then
             break
