c54cb1a6285f3bdd1788e8e89096ad90e4b549b9
Bruno FROGER <bruno.froger@orange.com>
2025-08-11 16:53:16 +0200

    mise a jour install nginx

--- 30-install-frontal_nginx.bash
+++ 30-install-frontal_nginx.bash
@@ -10,14 +10,61 @@ echo "*************************************************"
 if [[ "X-$K8S_FRONTAL_NGINX_INSTALLED" == "X-OK" ]]; then
   echo "installation nginx déjà réalisée"
 else 
-  cd k8sbfr-nginx
-  ls
-  kubectl apply -f k8sbfr-configMap-nginx.yaml
-  kubectl apply -f k8sbfr-deployement-nginx.yaml
-  kubectl apply -f k8sbfr-service-nginx.yaml
 
+  echo "-------------------------------------------------"
+  echo "installation de nginx ...."
+  sudo apt install -y nginx
+
+  echo "-------------------------------------------------"
+  echo "activation firewall ...."
+  sudo ufw --force enable
+
+  echo "-------------------------------------------------"
+  echo "activation de nginx ...."
+  sudo sudo systemctl enable nginx
+  sudo ufw allow 'Nginx HTTP'
+  sudo ufw allow 'OpenSSH'
+
+
+  echo "-------------------------------------------------"
+  echo "creation page par defaut nginx ...."
+  sudo mkdir /var/www/k8sbfr.zapto.org
+  sudo chmod 755 /var/www/k8sbfr.zapto.org
+  sudo mkdir /var/www/k8sbfr.zapto.org/html
+  sudo chmod 755 /var/www/k8sbfr.zapto.org/html
+
+  sudo -- sh -c 'echo "<html>
+        <head></head>
+        <body>
+        <h1>Bienvenue sur le cluster Kubernetes k8sbfr </h1>
+        </body>
+        </html>
+        " > /var/www/k8sbfr.zapto.org/html/index.html'
+  sudo chown -R www-data:www-data /var/www/k8sbfr.zapto.org
+
+  sudo -- sh -c 'echo "server {
+
+        listen 80;
+        listen [::]:80;
+
+        root /var/www/k8sbfr.zapto.org/html;
+
+        index index.html;
+        server_name k8sbfr.zapto.org localhost;
+
+        location / {
+            try_files $uri $uri/ =404;
+    }
+    }" \
+    > /etc/nginx/sites-available/k8sbfr.zapto.org'
+  sudo chown -R www-data:www-data /etc/nginx/sites-available/k8sbfr.zapto.org
+  sudo ln -s /etc/nginx/sites-available/k8sbfr.zapto.org /etc/nginx/sites-enabled/
+
+  echo "-------------------------------------------------"
+  echo "redemmarage nginx post config"
+  sudo systemctl restart nginx
+  
   echo "-------------------------------------------------"
   source set-bash-variable.bash K8S_FRONTAL_NGINX_INSTALLED="OK"
   echo "installation de nginx => fin"
-  cd ..
 fi
---
+++ 30-install-k8s_frontal_nginx.bash
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+
+echo "*************************************************"
+echo "*"
+echo "*       installation de nginx ...."
+echo "*"
+echo "*************************************************"
+
+if [[ "X-$K8S_FRONTAL_NGINX_INSTALLED" == "X-OK" ]]; then
+  echo "installation nginx déjà réalisée"
+else 
+  cd k8sbfr-nginx
+  kubectl apply -f k8sbfr-configMap-nginx.yaml
+  kubectl apply -f k8sbfr-deployement-nginx.yaml
+  kubectl apply -f k8sbfr-service-nginx.yaml
+
+  echo "-------------------------------------------------"
+  source set-bash-variable.bash K8S_FRONTAL_NGINX_INSTALLED="OK"
+  echo "installation de nginx => fin"
+  cd ..
+fi
