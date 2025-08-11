62593e0084a7624b1680783ddceed055bb063970
Bruno FROGER <bruno.froger@orange.com>
2025-08-11 14:01:03 +0200

    mise a jour install nginx

---
+++ 31-desinstall-nginx.bash
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+
+echo "*************************************************"
+echo "*"
+echo "*       desinstallation de nginx ...."
+echo "*"
+echo "*************************************************"
+
+if [[ "X-$K8S_FRONTAL_NGINX" == "X-OK" ]]; then
+else 
+  echo "docker n'est pas installé"
+
+  echo "-------------------------------------------------"
+  echo "arrêt de nginx ...."
+  sudo systemctl stop nginx
+  
+  echo "-------------------------------------------------"
+  sudo apt autoremove nginx -y
+  echo " A FINALISER"
+
+  echo "-------------------------------------------------"
+  echo "desactivation firewall ...."
+  sudo ufw disable
+
+  echo "-------------------------------------------------"
+  source set-bash-variable.bash K8S_FRONTAL_NGINX=""
+  echo "deinstallation de nginx => fin"
+fi
